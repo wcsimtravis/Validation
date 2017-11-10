b21fffdaa11f3db5acb720e223b55a7d75211b6b
brichards64 <b.richards@qmul.ac.uk.com>
2017-11-10 17:19:31 +0000

    test

--- Generate/Generate.sh
+++ Generate/Generate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export WCSim=$HYPERKDIR/WCSim
+WCSim=$HYPERKDIR/WCSim
 export LD_LIBRARY_PATH=$WCSim:$LD_LIBRARY_PATH
 
 g++ $ValidationPath/Generate/daq_readfilemain.C -o $ValidationPath/Generate/daq_readfilemain `root-config --libs --cflags` -L $WCSim -lWCSimRoot -I $WCSim/include
