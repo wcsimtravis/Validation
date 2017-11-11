57a79267d30d0caac26e51c0eae894aa6ce1bd07
brichards64 <b.richards@qmul.ac.uk.com>
2017-11-11 15:11:01 +0000

    test

--- Generate/Generate.sh
+++ Generate/Generate.sh
@@ -5,17 +5,5 @@ export LD_LIBRARY_PATH=$WCSim:$LD_LIBRARY_PATH
 
 g++ $ValidationPath/Generate/daq_readfilemain.C -o $ValidationPath/Generate/daq_readfilemain `root-config --libs --cflags` -L $WCSim -lWCSimRoot -I $WCSim/include
 
-i=0
-
-for file in `ls $ValidationPath/Generate/macReference/*.mac`
-do
-
-i=$(expr $i + 1)
-
-if [ $i -eq $1 ]
-then
-$WCSim/exe/bin/Linux-g++/WCSim $file
+$WCSim/exe/bin/Linux-g++/WCSim $1
 $ValidationPath/Generate/daq_readfilemain *.root
-fi
-
-done
